6a5e296fd326c3deb727e45e061f0d38f1c71d95
PRITHIKASELVAKUMAR <prithikaselvakumar123@gmail.com>
2025-09-12 18:49:32 +0530

    Add

--- Project/Backend/models/User.js
+++ Project/Backend/models/User.js
@@ -1,9 +1,9 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-  username: { type: String, required: true, unique: true }, // unique login name
-  email: { type: String, required: true, unique: true },    // optional but recommended
-  password: { type: String, required: true },               // hashed password
+  username: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
   role: { 
     type: String, 
     enum: ["farmer", "distributor", "retailer", "consumer"], 
