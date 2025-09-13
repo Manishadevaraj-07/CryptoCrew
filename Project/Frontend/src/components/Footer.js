7fe01272dc9ad05f66d4e71d5f1f30a22cf1c10d
arunnnn7 <arunnprakash7@gmail.com>
2025-09-13 21:15:56 +0530

    add

--- Project/Frontend/src/components/Footer.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function Footer() {
-    return (
-        <footer className="bg-gray-800 text-green-400 py-12 text-center">
-            <p>© 2024 AgriTrace. All rights reserved. Built with blockchain transparency in mind.</p>
-        </footer>
-    );
-}
