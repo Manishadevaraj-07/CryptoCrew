9c916dc30f7eec42f7967154e2c12cc11ab40f18
arunnnn7 <arunnprakash7@gmail.com>
2025-09-13 20:26:55 +0530

    add2

--- Project/Frontend/src/index.js
+++ Project/Frontend/src/index.js
@@ -2,11 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './tailwind.css';
-import './styles.css';
+import './styles.css';  // Make sure this is after tailwind.css
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
 );
